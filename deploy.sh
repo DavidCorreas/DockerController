0f97432eb7313a912e69daf066162b3fea98744e
JoseZamora97 <josezamora94@yahoo.es>
2020-04-19 21:16:27 +0200

    Mega change: Scaled load balancer and implemented MSQL master/ n-slave sharding

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Configuration
+APP_INSTANCES=3
+RABBIT_INSTANCES=2
+MYSQL_SLAVES_INSTACES=2
+
+# Clean the environment.
+docker-compose down -v --remove-orphans
+
+# Deploy the solution.
+docker-compose up --build \
+  --scale spring-app=$APP_INSTANCES \
+  --scale rabbit-api=$RABBIT_INSTANCES \
+  --scale mysql-slave=$MYSQL_SLAVES_INSTACES
+
+# Check the containers status.
+docker ps
